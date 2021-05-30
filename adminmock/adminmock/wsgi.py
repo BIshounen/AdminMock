35bbaebd4e11f6f72620c553c046dc90f51afd68
Nikita <n.batyanov@softpro.com>
2021-05-31 01:31:51 +0300

    migrations

--- adminmock/adminmock/wsgi.py
+++ adminmock/adminmock/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminmock.adminmock.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'adminmock.settings')
 
 application = get_wsgi_application()
