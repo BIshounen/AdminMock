afd0339b3e5c24e1a1459718f2ea5e928a8776e7
Nikita <n.batyanov@softpro.com>
2021-05-31 16:57:57 +0300

    changed games to table from multiselect

--- adminapp/serializers.py
+++ adminapp/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
-from adminapp.models import Employee
+from adminapp.models import Employee, Game
+
+
+class GameSerializer(serializers.HyperlinkedModelSerializer):
+
+    class Meta:
+
+        model = Game
+        fields = '__all__'
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-
+    employee_games = GameSerializer(read_only=True, many=True)
     class Meta:
 
         model = Employee
