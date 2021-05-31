c0573d192745d98b522f5991c810ec86d5d0a2d4
Nikita <n.batyanov@softpro.com>
2021-05-31 12:57:54 +0300

    rest test

--- adminapp/sereializers.py
+++ adminapp/sereializers.py
@@ -3,7 +3,8 @@ from adminapp.models import Employee, GAMES
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
+    my_field = fields.MultipleChoiceField(choices=GAMES)
     class Meta:
 
         model = Employee
-        fields = ['employee_name', fields.MultipleChoiceField(choices=GAMES)]
+        fields = ['employee_name']
