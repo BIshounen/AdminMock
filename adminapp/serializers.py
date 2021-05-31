b59fa78e2abc6272d221bf328fa75fec1d998bfd
Nikita <n.batyanov@softpro.com>
2021-05-31 17:11:29 +0300

    changed games to table from multiselect

--- adminapp/serializers.py
+++ adminapp/serializers.py
@@ -3,15 +3,17 @@ from adminapp.models import Employee, Game
 
 
 class GameSerializer(serializers.HyperlinkedModelSerializer):
+    def to_representation(self, value):
+        return value.name
 
     class Meta:
 
         model = Game
-        fields = '__all__'
 
 
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     employee_games = GameSerializer(read_only=True, many=True)
+
     class Meta:
 
         model = Employee
