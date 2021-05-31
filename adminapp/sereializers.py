19ecddaecc013255b0be115ae138ebca83e95567
Nikita <n.batyanov@softpro.com>
2021-05-31 12:41:26 +0300

    rest test

--- adminapp/sereializers.py
+++ adminapp/sereializers.py
@@ -6,4 +6,3 @@ class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
         fields = ['employee_name']
-        myField1 = fields.MultipleChoiceField(choices=GAMES)
