a365bef064fbf6227c9879260dc259eaf1cd5761
Nikita <n.batyanov@softpro.com>
2021-05-31 13:02:58 +0300

    rest test

--- adminapp/sereializers.py
+++ adminapp/sereializers.py
@@ -2,8 +2,26 @@ from rest_framework import serializers, fields
 from adminapp.models import Employee, GAMES
 
 
+class ChoiceField(serializers.ChoiceField):
+
+    def to_representation(self, obj):
+        if obj == '' and self.allow_blank:
+            return obj
+        return self._choices[obj]
+
+    def to_internal_value(self, data):
+        # To support inserts with the value
+        if data == '' and self.allow_blank:
+            return ''
+
+        for key, val in self._choices.items():
+            if val == data:
+                return key
+        self.fail('invalid_choice', input=data)
+
+
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
-    employee_games = fields.MultipleChoiceField(choices=GAMES)
+    employee_games = ChoiceField(choices=GAMES)
     class Meta:
 
         model = Employee
