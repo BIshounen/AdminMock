e615c488bf2af106adb0a96729a4e266d4c9f18b
Nikita <n.batyanov@softpro.com>
2021-06-16 14:00:54 +0300

    rest post test

--- adminapp/views.py
+++ adminapp/views.py
@@ -33,4 +33,4 @@ class GameViewSet(viewsets.ModelViewSet):
     API endpoint that allows users to be viewed or edited.
     """
     permission_classes = [permissions.IsAuthenticated]
-    queryset = []
+    queryset = Game.objects.none()
