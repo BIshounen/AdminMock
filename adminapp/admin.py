277fc33ccc4a4aca85bbd9a4934459e21fc2a8c2
Nikita <n.batyanov@softpro.com>
2021-06-18 10:19:25 +0300

    Presets intro

--- adminapp/admin.py
+++ adminapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from adminapp.models import Employee, Game, GamePreset
+from adminapp.models import Employee, Game, GamePreset, RussianPokerSettings, RussianPokerBonusTable
 
 # Register your models here.
 admin.site.register(Employee)
 admin.site.register(Game)
 admin.site.register(GamePreset)
+admin.site.register(RussianPokerSettings)
+admin.site.register(RussianPokerBonusTable)
